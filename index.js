36e37d2f03a2574307ddb66cf0d5fe7e02954b4f
Peakk2011 <peakk3984@gmail.com>
2024-05-05 22:06:36 +0700

    Store Sys 1.0

--- index.js
+++ index.js
@@ -33,15 +33,46 @@ function GetToggleOUST() {
 //     document.getElementById("NavbarVeryMainNo").style.top = ('0px')
 // }
 
-function changeCss () {
+function GetSearchOn() {
+    document.getElementById("SearchBox").style.top = "0px"
+    document.getElementById("SearchBox").style.opacity = "1"
+    if(this.scrollY > 350) {
+        document.getElementById("SearchBox").style.top = "58px"
+    }
+    // document.body.style.backdropFilter = "blur(20px)"
+    document.getElementById("LanguageToggle").style.opacity = "0"
+}
+
+function CloseSearch() {
+    document.getElementById("SearchBox").style.opacity = "0"
+    document.getElementById("SearchBox").style.top = "-140px"
+    // document.body.style.backdropFilter = "blur(0px)"
+}
+
+function LanguageSetup() {
+    document.getElementById("LanguageToggle").style.bottom = "58px"
+    document.getElementById("LanguageToggle").style.opacity = "1"
+    document.getElementById("SearchBox").style.opacity = "0"
+}
+
+function CloseLanguageSetup() {
+    document.getElementById("LanguageToggle").style.bottom = "-160px"
+    document.getElementById("LanguageToggle").style.opacity = "0"
+}
+
+function changeCss() {
     var navElement = document.querySelector("nav"); //after >
     this.scrollY > 450 ? navElement.style.opacity = 1 : navElement.style.opacity = 0;
+
+    var navElement = document.getElementById("SearchBox"); //after >
+    this.scrollY > 450 ? navElement.style.top = "58px" : navElement.style.opacity = "0"
 
     var HeaderPall = document.querySelector("header"); //after >
     this.scrollY > 350 ? HeaderPall.style.opacity = 0 : HeaderPall.style.opacity = 1;
 
 }
 
+changeCss()
   
 window.addEventListener("scroll", changeCss , false);
 
@@ -56,4 +87,3 @@ setTimeout(function() {
     document.getElementById('InsidePreLoa').style.opacity = "0"
     document.getElementById('InsidePreLoa').style.visibility = "hidden"
 }, 1500);
-
