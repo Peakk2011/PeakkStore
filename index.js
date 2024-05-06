767454c89f9c5b1c190c2b2af0fc1bc265d09d4b
Peakk2011 <peakk3984@gmail.com>
2024-05-06 14:05:36 +0700

    Undifinded Update Not Wait for Backend Login

--- index.js
+++ index.js
@@ -86,4 +86,60 @@ setTimeout(function() {
     document.getElementById('Preloader').style.width = "0px"
     document.getElementById('InsidePreLoa').style.opacity = "0"
     document.getElementById('InsidePreLoa').style.visibility = "hidden"
+    document.body.style.overflow = "scroll"
 }, 1500);
+
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+  
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 150;
+  
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+  }
+  
+  window.addEventListener("scroll", reveal);
+ 
+  const pressedButtonSelector = '[data-theme][aria-pressed="true"]';
+  const defaultTheme = 'green';
+  
+  const applyTheme = (theme) => {
+    const target = document.querySelector(`[data-theme="${theme}"]`);
+    document.documentElement.setAttribute("data-selected-theme", theme);
+    document.querySelector(pressedButtonSelector).setAttribute('aria-pressed', 'false');
+    target.setAttribute('aria-pressed', 'true');
+  };
+  
+  const handleThemeSelection = (event) => {
+    const target = event.target;
+    const isPressed = target.getAttribute('aria-pressed');
+    const theme = target.getAttribute('data-theme');        
+    
+    if(isPressed !== "true") {
+      applyTheme(theme);
+      localStorage.setItem('selected-theme', theme);
+    }
+  }
+  
+  const setInitialTheme = () => {
+    const savedTheme = localStorage.getItem('selected-theme');
+    if(savedTheme && savedTheme !== defaultTheme) {
+      applyTheme(savedTheme);
+    }
+  };
+  
+  setInitialTheme();
+  
+  const themeSwitcher = document.querySelector('.theme-switcher');
+  const buttons = themeSwitcher.querySelectorAll('button');
+  
+  buttons.forEach((button) => {
+     button.addEventListener('click', handleThemeSelection);
+  });
